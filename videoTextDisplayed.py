37e6c7945a50f0305eb2ae68841fdfd3d66c67ef
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-06-05 20:35:36 -0500

    Filter change

--- videoTextDisplayed.py
+++ videoTextDisplayed.py
@@ -1,32 +1,44 @@
 from PIL import Image
 from pytesseract import image_to_string
 import cv2
-import time
 import os
+import jellyfish
+
+
+def similitude(val1, val2):
+    return jellyfish.jaro_distance(unicode(val1), unicode(val2)) > 0.9
+
 
 ospath = os.path.dirname(__file__)
-img_path = (ospath + "/image.jpg")#.replace("/", "\\")
-vid_path = (ospath + "/01_how-can-i-succeed-in-this-course.mp4")#.replace("/", "\\")
+img_path = (ospath + "/image.jpg")  # .replace("/", "\\")
+vid_path = (ospath + "/01_how-can-i-succeed-in-this-course.mp4")  # .replace("/", "\\")
 
 print(img_path)
 print(vid_path)
 
+text = [""]
 cap = cv2.VideoCapture(vid_path)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
         try:
-            #cv2.imshow('frame', frame)
+            # cv2.imshow('frame', frame)
             cv2.imwrite(img_path, frame)
             img = Image.open(img_path)
-            #print(img)
-            print(image_to_string(img))
-            #print(image_to_string(img, lang='eng'))
-        except FileNotFoundError:
+            # print(img)
+            frame_text = image_to_string(img)
+            print(frame_text)
+            comp_text = text.pop()
+            text.append(comp_text)
+            if not similitude(frame_text, comp_text) and frame_text != "":
+                text.append(frame_text)
+            # print(image_to_string(img, lang='eng'))
+        except OSError:
             print("FILE NOT FOUND")
-        #time.sleep(5)
+        # time.sleep(5)
     else:
         break
 cap.release()
 cv2.destroyAllWindows()
+print(text)

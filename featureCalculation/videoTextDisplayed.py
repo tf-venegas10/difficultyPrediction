55d916fad61c4610dda47308af37550a377fa2a8
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-06-13 19:07:35 -0500

    Video transcript

--- featureCalculation/videoTextDisplayed.py
+++ featureCalculation/videoTextDisplayed.py
@@ -7,20 +7,40 @@ import sys
 import json
 import MySQLdb
 
+dbcomplete = MySQLdb.connect(host="qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+                             # your host, usually localhost
+                             user="znrmxn5ahxiedok5",  # your username
+                             passwd="r8lkor9pav5ag5uz",  # your password
+                             # port="3306",
+                             db="uzzonr2rx4qx8zu4")
 
-db = MySQLdb.connect(host="l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",    # your host, usually localhost
-                     user="gh7u6wguchfrkxo1",         # your username
-                     passwd="lqgvsrxvaeyb8uql", # your password
-                    # port="3306",
+db = MySQLdb.connect(host="l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",  # your host, usually localhost
+                     user="gh7u6wguchfrkxo1",  # your username
+                     passwd="lqgvsrxvaeyb8uql",  # your password
+                     # port="3306",
                      db="n501u8qclhvj0mdv")
+
+curcomplete = dbcomplete.cursor()
 cur = db.cursor()
 
 ids = {}
+
+curcomplete.execute("SELECT * FROM learning_resources;")
+for row in curcomplete.fetchall():
+    path = row[2].replace(
+        "/Users/rubenmanrique/Dropbox/DoctoradoAndes/Investigacion/Course Sequences Dataset/CourseraTexto/", "")
+    name = path.split(".")[0] + ".mp4"
+    ids[str(row[0])] = name
+
+dbcomplete.close()
+
+vids = {}
 cur.execute("SELECT * FROM VIDEO_QUALIFICATION  WHERE QUALIFICATION_AMOUNT>0")
 for row in cur.fetchall():
-    ids[str(row[0])] = 0
+    if str(row[0]) in ids:
+        vids[ids[str(row[0])]] = row[0]
 
-print(ids)
+print(vids)
 db.close()
 
 # sys.setdefaultencoding() does not exist, here!
@@ -38,7 +58,6 @@ rootdir = "E:\Coursera"
 export = open(ospath + '\InitialData\\video_caption_text.json', 'w')
 img_path = (ospath + "/InitialData/image.jpg")  # .replace("/", "\\")
 texts = []
-id = 1
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -49,8 +68,10 @@ for subdir, dirs, files in os.walk(rootdir):
         # print(img_path)
         # print(vid_path)
         if file.endswith(".mp4"):
-            print(id)
-            if str(id) in ids:
+            route = os.path.join(subdir, file)
+            name = route.replace("E:\Coursera\\", "").replace("\\", "/")
+            print(name)
+            if name in vids:
                 text = [""]
                 cap = cv2.VideoCapture(vid_path)
                 interval = 120
@@ -66,7 +87,7 @@ for subdir, dirs, files in os.walk(rootdir):
                                 img = Image.open(img_path)
                                 # print(img)
                                 frame_text = image_to_string(img)
-                                #print(frame_text)
+                                # print(frame_text)
                                 comp_text = text.pop()
                                 if not similitude(frame_text, comp_text) and frame_text != "":
                                     text.append(comp_text)
@@ -81,13 +102,12 @@ for subdir, dirs, files in os.walk(rootdir):
                             # time.sleep(5)
                         counti = counti + 1
                     else:
-                        #print(counti)
+                        # print(counti)
                         break
                 cap.release()
                 cv2.destroyAllWindows()
-                #print(text)
-                texts.append({'id': id, 'path': os.path.join(subdir, file), 'text': " ".join(text)})
-            id = id + 1
+                # print(text)
+                texts.append({'id': vids[name], 'path': name, 'text': " ".join(text)})
 conversion = json.dumps(texts, ensure_ascii=False)
 export.write(conversion)
 export.close()

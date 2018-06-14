69ab48e156e358dfab4d6c33f89403dcf2298fd3
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-06-13 21:49:05 -0500

    Video words count

--- featureCalculation/countVideoWords.py
+++ featureCalculation/countVideoWords.py
@@ -1,24 +1,24 @@
-import json
 from textstat.textstat import textstat
+import json
 
-# Feature id 8
+# Feature id 9
 
 transcriptFile = open('../InitialData/video_caption_text.json', 'r')
 lines = transcriptFile.read()
 transcript = json.loads(lines)
 
-file = open("Video_Phrases_Count.sql", "a")
+file = open("Video_Words_Count.sql", "a")
 
 for trans in transcript:
     text = trans['text']
     print(text)
     try:
-        value = textstat.sentence_count(text)
+        value = textstat.lexicon_count(text)
     except(UnicodeDecodeError):
         print "not read at id: " + str(trans['id'])
         # print row[1]
         value = 0
-    file.write("INSERT INTO FEATURES_PER_VIDEO (feature_id, video_id, value) VALUES (8, " + str(trans['id']) + ", " + str(
+    file.write("INSERT INTO FEATURES_PER_VIDEO (feature_id, video_id, value) VALUES (9, " + str(trans['id']) + ", " + str(
         value) + " );\n")
 
 file.close()

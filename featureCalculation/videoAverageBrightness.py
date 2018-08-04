016544443b519f34b41890fd5e59c71810c29fbd
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-08-04 07:10:18 -0500

    Visual features add

--- featureCalculation/videoAverageBrightness.py
+++ featureCalculation/videoAverageBrightness.py
@@ -1,8 +1,6 @@
-import numpy as np
-import imutils
+from PIL import Image
 import cv2
 import os
-from PIL import Image
 import sys
 import MySQLdb
 import re
@@ -24,28 +22,20 @@ def getVideoDurationSecs(path_to_video):
     return duration
 
 
-def image_colorfulness(image):
-    # split the image into its respective RGB components
-    (B, G, R) = cv2.split(image.astype("float"))
+def calculate_brightness(image):
+    greyscale_image = image.convert('L')
+    histogram = greyscale_image.histogram()
+    pixels = sum(histogram)
+    brightness = scale = len(histogram)
 
-    # compute rg = R - G
-    rg = np.absolute(R - G)
+    for index in range(0, scale):
+        ratio = histogram[index] / pixels
+        brightness += ratio * (-scale + index)
 
-    # compute yb = 0.5 * (R + G) - B
-    yb = np.absolute(0.5 * (R + G) - B)
-
-    # compute the mean and standard deviation of both `rg` and `yb`
-    (rbMean, rbStd) = (np.mean(rg), np.std(rg))
-    (ybMean, ybStd) = (np.mean(yb), np.std(yb))
-
-    # combine the mean and standard deviations
-    stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
-    meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
-
-    # derive the "colorfulness" metric and return it
-    return stdRoot + (0.3 * meanRoot)
+    return 1 if brightness == 255 else brightness / scale
 
 
+# DB connection with our dataset server
 dbcomplete = MySQLdb.connect(host="qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
                              # your host, usually localhost
                              user="znrmxn5ahxiedok5",  # your username
@@ -53,6 +43,7 @@ dbcomplete = MySQLdb.connect(host="qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.a
                              # port="3306",
                              db="uzzonr2rx4qx8zu4")
 
+# DB connection with our data server
 db = MySQLdb.connect(host="l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",  # your host, usually localhost
                      user="gh7u6wguchfrkxo1",  # your username
                      passwd="lqgvsrxvaeyb8uql",  # your password
@@ -64,6 +55,7 @@ cur = db.cursor()
 
 ids = {}
 
+# Path search
 curcomplete.execute("SELECT * FROM learning_resources;")
 for row in curcomplete.fetchall():
     path = row[2].replace(
@@ -87,14 +79,12 @@ db.close()
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
-ospath = os.path.dirname(__file__)
-ospath = ospath.replace("/featureCalculation", "")
+ospath = os.path.dirname(__file__).replace("/featureCalculation", "")
 rootdir = "E:/Coursera"
-export = open(ospath + '/InitialData/video_caption_text.json', 'w')
-img_path = (ospath + "/InitialData/image2.jpg")  # .replace("/", "\\")
+img_path = (ospath + "/InitialData/image.jpg")  # .replace("/", "\\")
 processed = 0
 
-output = open("Video_Average_Colorfulness.sql", "a")
+output = open("Video_Average_Brightness.sql", "a")
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -110,38 +100,39 @@ for subdir, dirs, files in os.walk(rootdir):
                 # if not key.endswith(".mp4"):
                 # print("THIS IS KEY: " + key)
                 if name.startswith(key):
-                    cap = cv2.VideoCapture(vid_path)
                     counti = 0
+                    cap = cv2.VideoCapture(vid_path)
                     duration = getVideoDurationSecs(vid_path)
                     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
                     print("Duration [s]: " + str(duration))
                     interval = int(math.floor((frame_count / duration) * 1))
                     print("Interval: " + str(interval))
-                    colorful = 0.0
-                    amount = 0
+                    average = 0.0
+                    amount = 0.0
 
                     while cap.isOpened():
                         ret, frame = cap.read()
                         if ret:
                             if counti % interval == 0:
                                 try:
-                                    # cv2.imshow('frame', frame)
                                     cv2.imwrite(img_path, frame)
-                                    img = cv2.imread(img_path)
-                                    img = imutils.resize(img, width=250)
-                                    colorful += image_colorfulness(img)
+                                    img = Image.open(img_path)
+                                    average += calculate_brightness(img)
                                     amount += 1
                                 except OSError:
                                     print("FILE NOT FOUND")
                         else:
                             break
                         counti = counti + 1
-                    average = colorful/float(amount)
-                    print("AVERAGE: "+str(average))
-                    output.write(
-                        "INSERT INTO FEATURES_PER_VIDEO (feature_id, video_id, value) VALUES (36, " + str(
-                            vids[key]) + ", " + str(average) + " );\n")
+                    if not amount == 0:
+                        average /= amount
+                    else:
+                        average = 0.0
+                    print("AVERAGE BRIGHTNESS: " + str(average))
                     processed = processed + 1
                     print("Processed: " + str(processed) + "/" + str(vids_amount))
                     cap.release()
                     cv2.destroyAllWindows()
+                    output.write(
+                        "INSERT INTO FEATURES_PER_VIDEO (feature_id, video_id, value) VALUES (38, " + str(
+                            vids[key]) + ", " + str(average) + " );\n")

c2fab0d12da33149c4c15ab0dd86ff7f3d8bda21
TOMAS FELIPE VENEGAS BERNAL <tf.venegas10@sis.virtual.uniandes.edu.co>
2018-09-26 10:04:50 -0500

    data without topics

--- FinalData/buildClassificationDataSMOTE.py
+++ FinalData/buildClassificationDataSMOTE.py
@@ -45,7 +45,7 @@ for row in cur.fetchall():
 cur.execute("SELECT FV.video_id, name, value, qualification " +
             "FROM FEATURES F JOIN FEATURES_PER_VIDEO FV ON F.ID=FV.FEATURE_ID " +
             "JOIN VIDEO_QUALIFICATION VQ ON FV.VIDEO_ID=VQ.VIDEO_ID " +
-            "WHERE QUALIFICATION<>0;")
+            "WHERE (QUALIFICATION<>0 and name not like '%programming:%' and name not like '%math:%');")
 
 for row in cur.fetchall():
     videos[row[0]][row[1]] = row[2]

39185ce52f4aaa894a304d3cb2cc26b8ed1dfe48
TOMAS FELIPE VENEGAS BERNAL <tf.venegas10@sis.virtual.uniandes.edu.co>
2018-10-16 17:55:59 -0500

    feature selection results

--- Testing/forestFeatureSelection.py
+++ Testing/forestFeatureSelection.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import RandomForestRegressor
 import MySQLdb
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 db = MySQLdb.connect(host="localhost",  # your host, usually localhost
@@ -9,19 +11,15 @@ db = MySQLdb.connect(host="localhost",  # your host, usually localhost
                          db="dajee")
 cur = db.cursor()
 cur.execute("SELECT * FROM VIDEO_QUALIFICATION WHERE QUALIFICATION_AMOUNT<>0;")
-temp = {}
 videos = {}
 for row in cur.fetchall():
-    temp[row[0]] = {}
-    temp[row[0]]["VIDEOID"] = row[0]
+    videos[row[0]] = {}
+    videos[row[0]]["VIDEOID"] = row[0]
 
-    # if (float(row[1]) < 2.34):
-    #     temp[row[0]]["qualification"] = "difficult"
-    # elif (float(row[1]) < 3.67):
-    #     temp[row[0]]["qualification"] = "intermediate"
-    # else:
-    #     temp[row[0]]["qualification"] = "easy"
-    temp[row[0]]["qualification"] = row[1]
+    if ((row[1]) == "Easy"):
+        videos[row[0]]["qualification"] = 0
+    else:
+        videos[row[0]]["qualification"] = 1
 
 cur.execute("SELECT FV.video_id, name, value, qualification " +
             "FROM FEATURES F JOIN FEATURES_PER_VIDEO FV ON F.ID=FV.FEATURE_ID " +
@@ -29,7 +27,7 @@ cur.execute("SELECT FV.video_id, name, value, qualification " +
             "WHERE QUALIFICATION_AMOUNT<>0;")
 
 for row in cur.fetchall():
-    temp[row[0]][row[1]] = row[2]
+    videos[row[0]][row[1]] = row[2]
 results = []
 test = []
 i = 1
@@ -37,26 +35,43 @@ toDo = True
 features = []
 xFeatures = []
 yScore = []
-
+print(videos.keys())
 for key in videos.keys():
     print(i)
     if toDo:
         toDo = False
         for feature in videos[key].keys():
-            if feature != "qualification":
+            if feature != "qualification" and feature!="VIDEOID":
                 features.append(feature)
     theseFeatures = []
 
     for feature in features:
         theseFeatures.append(videos[key][feature])
-    xFeatures.append(theseFeatures)
+    xFeatures.append(np.array(theseFeatures))
     yScore.append(videos[key]["qualification"])
     i += 1
-X = xFeatures
-Y = yScore
+X = np.array(xFeatures)
+Y = np.array(yScore)
+
+
 names = features
 rf = RandomForestRegressor()
 rf.fit(X, Y)
 print "Features sorted by their score:"
-print sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names),
+sortedList= sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), names),
              reverse=True)
+
+print(sortedList)
+
+x=[]
+y=[]
+csv= open("forestFeaturesSelectionResults.csv","w+")
+csv.write("Feature;Dispersion index\n")
+for i in xrange(len(sortedList)):
+   x.append(sortedList[i][1])
+   y.append(sortedList[i][0])
+   csv.write(str(sortedList[i][1])+";"+str(sortedList[i][0])+"\n")
+
+csv.close()
+plt.bar(x,y)
+plt.show()
